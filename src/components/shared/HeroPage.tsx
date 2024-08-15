7d4337542b3a37f13583dad39b026d49ea9acabc
AbdulazizRus <coderdim@gmail.com>
2024-08-15 12:30:20 +0500

    chanegs

--- src/components/shared/HeroPage.tsx
+++ src/components/shared/HeroPage.tsx
@@ -4,6 +4,28 @@ const HeroPage = () => {
   return (
     <div>
       <Swiper />
+      <div className="container py-3">
+        <div className="grid lg:grid-cols-4 grid-cols-2 gap-5 items-center">
+          <div>
+            <h1 className="text-2xl font-bold">3M+</h1>
+            <p className="text-neutral-500 text-lg">
+              Global investor community
+            </p>
+          </div>
+          <div>
+            <h1 className="text-2xl font-bold">2,500+</h1>
+            <p className="text-neutral-500 text-lg">Ventures supported</p>
+          </div>
+          <div>
+            <h1 className="text-2xl font-bold">31</h1>
+            <p className="text-neutral-500 text-lg">Unicorns in portfolio</p>
+          </div>
+          <div>
+            <h1 className="text-2xl font-bold">$2.6B+</h1>
+            <p className="text-neutral-500 text-lg">Capital raised</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/shared/MostTraction.tsx
@@ -0,0 +1,17 @@
+const MostTraction = () => {
+  return (
+    <div className="mt-20 container">
+      <div className="space-y-4">
+        <h1 className="text-3xl font-bold">Most traction</h1>
+        <p className="text-lg text-neutral-500">
+          The deals attracting the most interest right now
+        </p>
+        <div className="grid grid-cols-3">
+            
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MostTraction
