39470788496af9167db9f4a5b8704f8f43e9af66
AbdulazizRus <coderdim@gmail.com>
2024-08-15 22:19:27 +0500

    changes

---
+++ src/components/shared/SwiperPort.tsx
@@ -0,0 +1,45 @@
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/autoplay'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+
+const SwiperPort = () => {
+  return (
+    <>
+      <Swiper
+        // install Swiper modules
+        className="h-[70vh] relative"
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        slidesPerView={1}
+        breakpoints={{
+          640: {
+            slidesPerView: 1.5,
+          },
+        }}
+        spaceBetween={50}
+        loop={true}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide>
+          <img src="/b1.png" className="rounded-3xl w-full h-full" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/b2.png" className="rounded-3xl w-full h-full" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/b3.png" className="rounded-3xl w-full h-full" alt="" />
+        </SwiperSlide>
+        <div className="absolute z-50 w-[150px] bg-gradient-to-l from-white/90 to-transparent pointer-events-none top-0 right-0 h-full "></div>
+      </Swiper>
+    </>
+  )
+}
+
+export default SwiperPort
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import HeroPage from '../components/shared/HeroPage'
 import MostTraction from '../components/shared/MostTraction'
+import PortfolioWins from '../components/shared/PortfolioWins'
 
 const Home = () => {
   return (
     <main className="pt-20">
       <HeroPage />
       <MostTraction />
+      <PortfolioWins />
     </main>
   )
 }
